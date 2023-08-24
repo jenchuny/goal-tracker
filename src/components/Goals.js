58edef64344088ee9d1cc40a776b14ef0219c578
churian <jennychu95@gmail.com>
2023-08-24 15:54:54 +1000

    goals table styling

--- src/components/Goals.js
+++ src/components/Goals.js
@@ -37,25 +37,61 @@ function Goals() {
     }
   };
 
-  return (
-    <div class="container mx-auto">
-      <h2 className="text-xl font-bold mb-4">Goals</h2>
-      <ul>
-        {goals.map((goal) => (
-          <li key={goal.id}>{goal.text}</li>
-        ))}
-      </ul>
+  const GoalsTable = ({ goals }) => {
+    return (
+      <div className="flex flex-co py-4">
+        <div className="-m-1.5 overflow-x-auto">
+          <div className="p-1.5 min-w-full inline-block align-middle">
+            <div className="border rounded-lg overflow-hidden dark:border-gray-700">
+              <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                <thead>
+                  <tr>
+                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Goal</th>
+                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Age</th>
+                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Address</th>
+                    <th scope="col" className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase">Action</th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
+                  {goals.map((goal) => (
+                    <tr key={goal.id}>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">{goal.text}</td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">Age Value</td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">Address Value</td>
+                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                        <a className="text-blue-500 hover:text-blue-700" href="#">Delete</a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
 
+  return (
+<div className="container mx-auto py-4">
+
+<div class="flex rounded-md shadow-sm">
       <input
         type="text"
-        class="py-3 px-5 block w-full border-gray-200 rounded-full text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400" 
+        id="hs-trailing-button-add-on" 
+        name="hs-trailing-button-add-on" 
+        class="py-3 px-4 block w-full border-gray-200 shadow-sm rounded-l-md text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400" 
         placeholder="Enter your goal"
         value={newGoal}
         onChange={(e) => setNewGoal(e.target.value)}
       />
-      <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded" onClick={handleAddGoal}>Add Goal</button>
+      <button class="py-3 px-4 inline-flex flex-shrink-0 justify-center items-center gap-2 rounded-r-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all text-sm" onClick={handleAddGoal}>Add Goal</button>
     </div>
+
+    <GoalsTable goals={goals} />
     
+    </div>
+
   );
 }
 
