7eeeb98cb410514b2b8cba70ad36ea6d5298ee23
churian <jennychu95@gmail.com>
2023-09-26 14:06:36 +1000

    fixed fetchPoints

--- src/components/Points.js
+++ src/components/Points.js
@@ -1,35 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from './AuthContext';
-import { addDoc, getDocs, query, orderBy, where } from 'firebase/firestore';
-import { db, userCollection } from '../firebaseUtils'; 
+import { getDocs, query, where } from 'firebase/firestore';
+import { userCollection } from '../firebaseUtils'; 
 
 function Points() {
   const { authUser } = useAuth(); 
   const [points, setPoints] = useState({
-    assignedPoints: 0,
-    redeemedPoints: 0,
+    pointsEarned: 0,
+    pointsRedeemed: 0,
   });
 
   useEffect(() => {
     if (authUser) {
       fetchPoints(authUser.uid);
     }
-  }, []); // Empty dependency array for initial load
-  
+  }, [authUser]);
 
   const fetchPoints = async (userId) => {
     try {
-      if (!authUser) return; // Return early if no user is logged in
-  
-      const q = query(userCollection, where('userId', '==', userId), orderBy('timestamp', 'desc'));
-      const querySnapshot = await getDocs(q);
-      const pointsData = [];
-  
-      querySnapshot.forEach((doc) => {
-        pointsData.push({ id: doc.id, ...doc.data() });
-      });
+      if (!authUser) return; // Return early if no user is not logged in
 
-      setPoints(pointsData); // Update the state with fetched goals
+      const q = query(userCollection, where('userId', '==', userId));
+      const querySnapshot = await getDocs(q);
+
+      if (querySnapshot.size > 0) {
+        // Assuming there's only one matching document for the user
+        const userDoc = querySnapshot.docs[0];
+        const userData = userDoc.data();
+
+        setPoints({
+          pointsEarned: userData.pointsEarned || 0,
+          pointsRedeemed: userData.pointsRedeemed || 0,
+        });
+      } else {
+        // Handle the case where there's no matching user document
+        console.log('User document not found.');
+      }
     } catch (error) {
       console.error('Error fetching points: ', error);
     }
@@ -42,6 +48,6 @@ function Points() {
       <p>Redeemed Points: {points.pointsRedeemed}</p>
     </div>
   );
-};
+}
 
 export default Points;
