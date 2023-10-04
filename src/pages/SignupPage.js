2962e81cd7535eebec35df617218b915bfadece8
churian <jennychu95@gmail.com>
2023-10-04 22:02:30 +1100

    fixed auth styling

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../firebaseUtils';
 import { Link, useNavigate } from 'react-router-dom';
+import '../App.css';
 
 function SignUpPage() {
   const [email, setEmail] = useState("");
@@ -23,23 +24,17 @@ function SignUpPage() {
   };
 
   return (
-    <html class="h-full">
-    <body class="dark:bg-slate-900 bg-gray-100 flex h-full items-center py-16">
+    <html class="w-full">
+    <body class="dark:bg-slate-900 bg-gray-100 w-full pt-10 px-4 sm:px-6 md:px-8 lg:pl-72"> 
       <main class="w-full max-w-md mx-auto p-6">
-        <div class="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+        <div class="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700 grid">
           <div class="p-4 sm:p-7">
-            <div class="text-center">
-              <h1 class="block text-2xl font-bold text-gray-800 dark:text-white">Sign up</h1>
-              <p class="mt-2 text-sm text-gray-600 dark:text-gray-400">
-                Already have an account? 
-                <Link class="text-blue-600 decoration-2 hover:underline font-medium" to="/login">
-                  Sign in here
-                </Link>
-              </p>
+            <div class="text-left">
+              <h1 class="block text-3xl font-bold text-gray-800 dark:text-white">Get started! 💪</h1>
             </div>  
               <form onSubmit={signUp}>
-                <div class="grid gap-y-4">
-                  <div>
+              <div class="grid">
+                    <div class="relative mb-5">
                     <label for="email" class="block text-sm mb-2 dark:text-white">Email address</label>
                     <div class="relative">
                       <input type="email" id="email" name="email" class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" required aria-describedby="email-error" value={email} onChange={(e) => setEmail(e.target.value)}></input>
@@ -49,12 +44,12 @@ function SignUpPage() {
                         </svg>
                       </div>
                     </div>
-                    <p class="hidden text-xs text-red-600 mt-2" id="email-error">Please include a valid email address so we can get back to you</p>
+                    <p class="hidden text-xs text-red-600 mt-2" id="email-error">Please include a valid email address.</p>
                   </div>
 
                   <div>
                     <label for="password" class="block text-sm mb-2 dark:text-white">Password</label>
-                    <div class="relative">
+                    <div class="relative mb-5">
                       <input type="password" id="password" name="password" class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" required aria-describedby="password-error" value={password} onChange={(e) => setPassword(e.target.value)}></input>
                       <div class="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
                         <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
@@ -65,9 +60,16 @@ function SignUpPage() {
                     <p class="hidden text-xs text-red-600 mt-2" id="password-error">8+ characters required</p>
                   </div>
   
-                  <button type="submit" class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">Sign up</button>
+                  <button type="submit" class="py-3 px-4 mb-5 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-slate-950 text-white hover:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">Sign up</button>
                 </div>
+                
               </form>
+              <p class="mt-2 text-sm text-gray-600 dark:text-gray-400">
+                Already have an account? 
+                <Link class="text-orange-600 decoration-2 hover:underline font-medium" to="/login">
+                  Sign in here
+                </Link>
+              </p>
             </div>
         </div>
       </main>
