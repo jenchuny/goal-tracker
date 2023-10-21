6dab40b16ec15c10026d90474d0e8589f42fb5a6
churian <jennychu95@gmail.com>
2023-10-22 10:46:45 +1100

    todocard styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import clsx from 'clsx'
 
-export const TodoCard = ({ title, checked, points }) => {
+export const TodoCard = ({ title, checked, points, goalId, currentStatus, handleChangeGoalStatus }) => {
     const [check, setChecked] = React.useState(checked);
+    const [status, setStatus] = React.useState(currentStatus);
   
-    const unselectedStyle = "bg-gray-900 rounded-xl p-4";
-    const selectedStyle = "bg-purple-400 rounded-xl p-4";
+    const unselectedStyle = "bg-slate-900 rounded-xl p-4";
+    const selectedStyle = "bg-orange-600 rounded-xl p-4";
+  
+    const handleStatusChange = () => {
+        const newStatus = status === 'complete' ? 'incomplete' : 'complete';
+        handleChangeGoalStatus(goalId, newStatus);
+        setStatus(newStatus);
+        setChecked(newStatus === 'complete');
+    };
   
     return (
       <div className={clsx(check ? selectedStyle : unselectedStyle)}>
         <div className="flex space-x-4 items-center mb-4">
-          <input type="checkbox" checked={check} onClick={() => { setChecked(!check) }} />
+          <input type="checkbox" checked={check} onChange={handleStatusChange} />
           <div className={clsx("text-white", check && "line-through")}>{title}</div>
         </div>
-        <div className={clsx(check ? "text-white" : "text-purple-400")}>{points} points</div>
+        <div className={clsx(check ? "text-white" : "text-orange-400")}>{points} points</div>
       </div>
     );
   };
