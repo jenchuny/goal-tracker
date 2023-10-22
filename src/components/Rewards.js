adeb53f9cb6deca73eeda7e69335b56c83382a49
churian <jennychu95@gmail.com>
2023-10-22 15:47:39 +1100

    z index

--- src/components/Rewards.js
+++ src/components/Rewards.js
@@ -153,7 +153,7 @@ function Rewards() {
       </div>
 
       {showModal ? (
-        <div className="fixed z-10 inset-0 overflow-y-auto mb-5" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <div className="fixed z-70 inset-0 overflow-y-auto mb-5" aria-labelledby="modal-title" role="dialog" aria-modal="true">
           <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
             <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity mb-5" aria-hidden="true"></div>
             <span className="hidden sm:inline-block sm:align-middle sm:h-screen mb-5" aria-hidden="true">&#8203;</span>
