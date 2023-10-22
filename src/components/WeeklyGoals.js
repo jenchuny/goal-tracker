6586a3fc70383bb49e4a179dc4251eaebb28764d
churian <jennychu95@gmail.com>
2023-10-22 14:33:37 +1100

    modal overlay fix

--- src/components/WeeklyGoals.js
+++ src/components/WeeklyGoals.js
@@ -186,7 +186,7 @@ return (
       {showModal ? (
         <div className="fixed z-10 inset-0 overflow-y-auto mb-5" aria-labelledby="modal-title" role="dialog" aria-modal="true">
           <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-            <div className="fixed inset-0 bg-white bg-opacity-75 transition-opacity mb-5" aria-hidden="true"></div>
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity mb-5" aria-hidden="true"></div>
             <span className="hidden sm:inline-block sm:align-middle sm:h-screen mb-5" aria-hidden="true">&#8203;</span>
             <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
               <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
