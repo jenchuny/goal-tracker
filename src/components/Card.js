788abe742ec5255b26c6e083cc860b601e88bca9
churian <jennychu95@gmail.com>
2023-10-22 12:11:16 +1100

    white text on orange

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,7 @@ export const TodoCard = ({ title, status, points, goalId, handleChangeGoalStatus
   }, [status]);
   
   const unselectedStyle = "bg-white rounded-xl p-4";
-  const selectedStyle = "bg-orange-600 rounded-xl p-4";
+  const selectedStyle = "bg-orange-600 rounded-xl p-4 text-white";
 
   const handleStatusChange = () => {
     const newStatus = check ? 'incomplete' : 'complete';
@@ -22,9 +22,9 @@ export const TodoCard = ({ title, status, points, goalId, handleChangeGoalStatus
       <div className="md:p-5">
         <div className="flex space-x-4 items-center mb-4">
           <input type="checkbox" checked={check} onChange={handleStatusChange} />
-          <h3 className={`text-lg font-bold text-gray-800 dark:text-white ${clsx(check && "line-through")}`}>{title}</h3>
+          <h3 className={`text-lg font-bold ${clsx(check ? "text-white line-through" : "text-gray-800 dark:text-white")}`}>{title}</h3>
         </div>
-        <p className={`mt-2 text-gray-800 dark:text-gray-400 ${clsx(check ? "text-slate" : "text-orange-400")}`}><b>{points} points</b></p>
+        <p className={`mt-2 ${clsx(check ? "text-white" : "text-gray-800 dark:text-gray-400")}`}><b>{points} points</b></p>
       </div>
     </div>
   );
